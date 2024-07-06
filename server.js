b7362f47e45be06577d1a495d26ebade942284f9
Jay <gajjar.jay2546@gmail.com>
2024-07-06 01:22:18 -0600

    Ready to deploy again with the secure and Changes new

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
   cookie: {
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
     httpOnly: true,
